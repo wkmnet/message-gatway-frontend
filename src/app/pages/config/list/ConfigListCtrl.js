5d918aa1f56d196dd1fc8e293a55eb12f1353a7c
qinxiangyu <qinxiangyu@xinpianchang.com>
2017-12-15 10:41:22 +0800

    系统参数表

--- src/app/pages/config/list/ConfigListCtrl.js
+++ src/app/pages/config/list/ConfigListCtrl.js
@@ -81,45 +81,6 @@
         };
         $scope.queryCurrent();
         
-/*
-        $scope.currentUser= {};
-        $scope.queryCurrent = function (id) {
-            $http.get("/api/user/" + id).success(function(resp){
-                if(resp.success){
-                    $scope.currentUser = resp.data;
-                } else {
-                    toastr.error(resp.message);
-                }
-            }).error(function(resp,status){
-                console.log("status:",status);
-                toastr.error(resp);
-            });
-        };
-
-
-      $scope.getCookie = function(){
-            var name = "MANGER-USER-KEY";
-            var cookie_value =  $scope.readCookie(name);
-            var end = cookie_value.indexOf("-")
-            console.log("value : " + cookie_value);
-            var id = cookie_value.substring(0,end);
-            console.log("currentId : " + id);
-            $scope.queryCurrent(id);
-        };
-
-        $scope.readCookie = function(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for(var i=0;i < ca.length;i++) {
-                var c = ca[i];
-                while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-            }
-            return null;
-        }
-
-        $scope.getCookie();
-*/
 
     }
 
